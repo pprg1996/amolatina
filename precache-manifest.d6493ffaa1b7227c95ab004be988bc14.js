4c1fa0ce54c1f75fa2a4b4e262da8a13d3d8d148
pprg1996 <pprg1996@gmail.com>
2020-06-20 09:25:53 -0400

    Build 1

---
+++ precache-manifest.d6493ffaa1b7227c95ab004be988bc14.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "600f3d6472067b6cd1131ecf4bf88dfc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2616abd46472b274858a",
+    "url": "/static/css/2.98c15a95.chunk.css"
+  },
+  {
+    "revision": "1fa0b31ca5061b582aaf",
+    "url": "/static/css/main.0defd9b9.chunk.css"
+  },
+  {
+    "revision": "2616abd46472b274858a",
+    "url": "/static/js/2.ccd9318e.chunk.js"
+  },
+  {
+    "revision": "f3f993d62d1b48701ad9c1ece5582ac9",
+    "url": "/static/js/2.ccd9318e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fa0b31ca5061b582aaf",
+    "url": "/static/js/main.e74bd6fb.chunk.js"
+  },
+  {
+    "revision": "0fde9cc089529f3d3686",
+    "url": "/static/js/runtime-main.5558a57b.js"
+  },
+  {
+    "revision": "9a3f762e28e3e049560761fbfe1f3208",
+    "url": "/static/media/award.9a3f762e.svg"
+  },
+  {
+    "revision": "cb487b2d54389f0c98da96bdb9b72d39",
+    "url": "/static/media/cancel.cb487b2d.svg"
+  },
+  {
+    "revision": "2149edd2920b93e2e0799b35d3781713",
+    "url": "/static/media/carta.2149edd2.svg"
+  },
+  {
+    "revision": "e4f98c5c29b48eea0042e30a0cc55d66",
+    "url": "/static/media/chat.e4f98c5c.svg"
+  },
+  {
+    "revision": "302cf18824e21bdfd98f4d5d2a30e486",
+    "url": "/static/media/checkmark.302cf188.svg"
+  },
+  {
+    "revision": "33eb8d30426855db9309884404a06b24",
+    "url": "/static/media/chevron.33eb8d30.svg"
+  },
+  {
+    "revision": "963b59a5ff74d657fa4c8df5d0cdc444",
+    "url": "/static/media/crown.963b59a5.svg"
+  },
+  {
+    "revision": "01926ac6c107f62a54374d0e32b2945c",
+    "url": "/static/media/eliminar-multa.01926ac6.svg"
+  },
+  {
+    "revision": "f1306e88e7cdfebc1cff4c10770e8c34",
+    "url": "/static/media/email-arrow.f1306e88.svg"
+  },
+  {
+    "revision": "07aaf4e1a13cee1dae207fb05a0175f5",
+    "url": "/static/media/expand.07aaf4e1.svg"
+  },
+  {
+    "revision": "13f250b495c8712b41993caf6d3f75ca",
+    "url": "/static/media/foto.13f250b4.svg"
+  },
+  {
+    "revision": "e8d650681c0f20f3e8a4f7e840c2714f",
+    "url": "/static/media/foto2.e8d65068.svg"
+  },
+  {
+    "revision": "7d4693d232140d7aaf1a3fb0972e4927",
+    "url": "/static/media/heart.7d4693d2.svg"
+  },
+  {
+    "revision": "2b07aa8709512c7573157229f6280f39",
+    "url": "/static/media/left.2b07aa87.svg"
+  },
+  {
+    "revision": "1265cb6730a180aac4bfd58cbfd204ef",
+    "url": "/static/media/liberar.1265cb67.svg"
+  },
+  {
+    "revision": "c67672355c245605873951c420b2568c",
+    "url": "/static/media/logout.c6767235.svg"
+  },
+  {
+    "revision": "cf78973c4b6ba97796aa2dd1bc5b5bea",
+    "url": "/static/media/mensaje.cf78973c.svg"
+  },
+  {
+    "revision": "14a9ed3ad17b93c779630e7005a8b80a",
+    "url": "/static/media/menu.14a9ed3a.svg"
+  },
+  {
+    "revision": "e312402ad7e70223607b06e5c95bdd2b",
+    "url": "/static/media/multa.e312402a.svg"
+  },
+  {
+    "revision": "b85a4bc5e90fa2140c90424fd1f3541f",
+    "url": "/static/media/offline.b85a4bc5.svg"
+  },
+  {
+    "revision": "05ae52e2dd079566960a86e4821f7992",
+    "url": "/static/media/registrar.05ae52e2.svg"
+  },
+  {
+    "revision": "aa5a5a24ad259f6bd1b2e0a3c2bd2375",
+    "url": "/static/media/spinner.aa5a5a24.svg"
+  },
+  {
+    "revision": "4a435e0296b01d43b3ba1d5e4f6ffd55",
+    "url": "/static/media/star.4a435e02.svg"
+  },
+  {
+    "revision": "db19c569f9e6d7985339c5fde896fc7a",
+    "url": "/static/media/starStroke.db19c569.svg"
+  },
+  {
+    "revision": "00756f070706d85b02a80825c5650904",
+    "url": "/static/media/subir-foto.00756f07.svg"
+  },
+  {
+    "revision": "1566f00c4ba5888d4417632c818ef35f",
+    "url": "/static/media/tareas.1566f00c.svg"
+  },
+  {
+    "revision": "b8870a3be62650279d38ba92cdfd6bd4",
+    "url": "/static/media/video.b8870a3b.svg"
+  },
+  {
+    "revision": "afc652b8ec764df7e35edf456b4992a7",
+    "url": "/static/media/video2.afc652b8.svg"
+  }
+]);
